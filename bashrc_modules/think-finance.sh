6de3410558a6b381ff262d08e2471143a893b9ff
Nathan Kowald <nkowald@uk.thinkfinance.com>
2013-10-11 08:39:06 +0100

    many updates including REBASELINE!

--- bashrc_modules/think-finance.sh
+++ bashrc_modules/think-finance.sh
@@ -32,7 +32,6 @@ alias tests='cd /var/www/html/tests'
 alias sql='cd /var/www/html/sql'
 alias lb='svn ls ${URL_BRANCH_ROOT} --verbose'
 alias mb='lb | grep nkowald'
-alias swt='cd /var/www/html && switchTrunk'
 alias restart_apache='sudo /etc/init.d/crond stop && sudo service httpd stop && sudo service httpd start && sudo /etc/init.d/crond start'
 alias restart_mysql='sudo /sbin/service mysql restart'
 alias ephpi='sudo vim /etc/php.ini'
@@ -78,9 +77,26 @@ function switchTrunk() {
 	svn switch ${URL_TRUNK_ROOT}
 }
 export -f switchTrunk
+alias swt='sites && switchTrunk'
+
+# Switch to develop
+function switchDevelop() {
+	# Check for uncommitted files
+	if getUncommittedFiles
+	then
+		printf "$MSG_FAIL You have uncommitted files. You must commit these files before switching:\n\n"
+		svn status
+		printf "\n"
+		return 0
+	fi
+
+	svn switch ${URL_DEVELOP_ROOT}
+}
+export -f switchDevelop
+# Switch SVN branch
+alias swd='sites && switchDevelop'
 
 # Switch SVN branch
-alias sb='cd /var/www/html && switchBranch'
 function switchBranch() {
 	# Branch name is required
 	if [ $# -eq 0 ]
@@ -109,6 +125,7 @@ function switchBranch() {
 	printf "${GREEN}Successfully switched to branch:\n${NORMAL}${URL_BRANCH_ROOT}$1\n"
 }
 export -f switchBranch
+alias sb='sites && switchBranch'
 
 # Get the name of the current branch
 alias wb='getBranchName'
@@ -132,12 +149,12 @@ function getBranchNumberFromName() {
 export -f getBranchNumberFromName
 
 # Get Branch URL
-alias wbu=getBranchURL
 function getBranchURL() {
 	DIR_ROOT=$(getRootFromDir)
 	echo $(cd $DIR_ROOT && svn info | grep 'URL: ' | grep -oEi 'http.+')
 }
 export -f getBranchURL
+alias wbu=getBranchURL
 
 # Get the Target Process URL associated with the current branch
 function getTPURL {
@@ -148,9 +165,7 @@ function getTPURL {
 }
 export -f getTPURL
 
-
 # Commit code
-alias commit='commitCode'
 function commitCode() {
 	# Switch to branch root 
 	DIR_ROOT=$(getRootFromDir)
@@ -165,8 +180,14 @@ function commitCode() {
 		return 0
 	fi
 
+    QUIET=0
+	if [[ $2 && $2 = --quiet ]]
+	then
+        QUIET=1
+	fi
+
 	# Prompt about PHP Code Sniffer if that exists
-	if type -p phpcs > /dev/null;
+	if [[ $QUIET -eq 0 ]] && type -p phpcs > /dev/null;
 	then
 		PHP_FILES=$(echo $STATUS | tr ' ' '\n' | grep -E '*.php')
 		NO_FILES=$(echo $PHP_FILES | grep -v '^\s*$' | wc -l)
@@ -210,8 +231,14 @@ function commitCode() {
 	then
 		BRANCH_NO=$(getBranchNumberFromName $BRANCH)
 		printf "\n"
-		read -p "Enter your commit comment: " COMMENT
-		COMMIT_COMMENT="#$BRANCH_NO comment: $COMMENT" 
+	    if [ $# -eq 0 ]
+        then 
+		    read -p "Enter your commit comment: " COMMENT
+		    COMMIT_COMMENT="#$BRANCH_NO comment: $COMMENT" 
+        else
+            COMMIT_COMMENT=$1
+        fi
+
 		printf "${CYAN}\n$COMMIT_COMMENT${NORMAL}\n\n"
 
 		read -p "Commit with the following comment? (y/n) "
@@ -240,9 +267,9 @@ function commitCode() {
 	fi
 }
 export -f commitCode
+alias commit='commitCode'
 
 # Create new SVN branch based on Trunk
-alias nb='cd /var/www/html && newBranch'
 function newBranch() {
 
 	MSG_FORCE="${CYAN}Hint: To create a branch named '$1', add the --force flag: 'nb $1 --force'\n"
@@ -313,6 +340,7 @@ function newBranch() {
 	fi
 }
 export -f newBranch
+alias nb='sites && newBranch'
 
 # Gets the dir root for the given branch (e.g. /var/www/html)
 function getRootFromDir()
@@ -348,8 +376,202 @@ function doesBranchExist()
     fi
 }
 export -f doesBranchExist
-alias be='cd /var/www/html/ && doesBranchExist'
+alias be='sites && doesBranchExist'
 
+function getVersionNumber()
+{
+    echo $(getBranchName | grep -o '[0-9]*$')
+}
+export -f getVersionNumber
+
+function getNextVersionNumber()
+{
+    NEXT_VERSION_NUM=$(getVersionNumber)
+    ((NEXT_VERSION_NUM++))
+    echo $NEXT_VERSION_NUM
+}
+export -f getNextVersionNumber
+
+function getAllRevisionsFromBranch()
+{
+	if [ $# -eq 0 ]
+	then
+		printf "$MSG_FAIL Branch name is required.\n$MSG_USAGE getAllRevisionsFromBranch $EXAMPLE_BRANCH\n"
+		return 0
+	fi
+	BRANCH=$1
+    echo $(svn log --stop-on-copy "${URL_BRANCH_ROOT}${BRANCH}" | grep -Po '^r[^ ]+')
+}
+
+function getRevisionRange()
+{
+	if [ $# -eq 0 ]
+	then
+		printf "$MSG_FAIL Branch name is required.\n$MSG_USAGE getFirstLastRevision $EXAMPLE_BRANCH\n"
+		return 0
+	fi
+	BRANCH=$1
+    ALL_REVS=$(getAllRevisionsFromBranch $1)
+    # Replace spaces with newlines
+    REV_LINES=$(echo $ALL_REVS | tr ' ' '\n' | sed -n '1p;$p' | sort | uniq | tr -d 'r')
+    echo $REV_LINES
+}
+export -f getRevisionRange
+
+function getRevisionCMD()
+{
+	if [ $# -eq 0 ]
+	then
+		printf "$MSG_FAIL Branch name is required.\n$MSG_USAGE getRevisionCMD $EXAMPLE_BRANCH\n"
+		return 0
+	fi
+	BRANCH=$1
+
+    REV_RANGE=$(getRevisionRange $BRANCH)
+    REV_ARRAY=($REV_RANGE)
+    if [[ ${#REV_ARRAY[@]} -eq 2 ]]
+    then
+        # Annoyingly, you have to specify REV-1 if you want that REV to be merged.
+        FIRST_REV=${REV_ARRAY[0]}
+        ((FIRST_REV--))
+        REV_RANGE=$FIRST_REV:${REV_ARRAY[1]}
+    else
+        REV_RANGE="-c ${REV_ARRAY[0]}"
+    fi
+
+    echo $REV_RANGE
+}
+export -f getRevisionCMD
+
+function getRevisionRangeForComment()
+{
+	if [ $# -eq 0 ]
+	then
+		printf "$MSG_FAIL Branch name is required.\n$MSG_USAGE getRevisionCMD $EXAMPLE_BRANCH\n"
+		return 0
+	fi
+	BRANCH=$1
+
+    REV_RANGE=$(getRevisionRange $BRANCH)
+    REV_ARRAY=($REV_RANGE)
+    if [[ ${#REV_ARRAY[@]} -eq 2 ]]
+    then
+        FIRST_REV=${REV_ARRAY[0]}
+        ((FIRST_REV--))
+        REV_RANGE=${REV_ARRAY[0]}-${REV_ARRAY[1]}
+    else
+        REV_RANGE="${REV_ARRAY[0]}"
+    fi
+
+    echo $REV_RANGE
+}
+export -f getRevisionRangeForComment
+
+function getCommitInfoFromBranch()
+{
+	if [ $# -eq 0 ]
+	then
+		printf "$MSG_FAIL Branch name is required.\n$MSG_USAGE getFirstLastRevision $EXAMPLE_BRANCH\n"
+		return 0
+	fi
+	BRANCH=$1
+    echo $(svn log --stop-on-copy "${URL_BRANCH_ROOT}${BRANCH}" | grep -Po '^r[^ ]+')
+}
+export -f getCommitInfoFromBranch
+alias gci='getCommitInfoFromBranch'
+
+function viewHistory()
+{
+    BRANCH_NAME=$(getBranchName)
+    svn log -v --stop-on-copy ${URL_BRANCH_ROOT}${BRANCH_NAME}
+}
+export -f viewHistory
+alias vh='viewHistory'
+
+function rebaseline()
+{
+    THIS_BRANCH=$(getBranchName)
+    NEXT_VERSION_NUM=$(getNextVersionNumber)
+    NEXT_BRANCH_NAME=$(getBranchName | grep -o '.*_')${NEXT_VERSION_NUM}
+    BRANCH_NO=$(getBranchNumberFromName ${THIS_BRANCH})
+
+    # Show nice summary of changes
+    printf "${GREEN}Commit log:${NORMAL} ${THIS_BRANCH}\n"
+    svn log -v --stop-on-copy ${URL_BRANCH_ROOT}${THIS_BRANCH}
+    printf "\n"
+    read -p "Create ${YELLOW}${NEXT_BRANCH_NAME}${NORMAL} and merge ${GREEN}all commits${NORMAL} from '${CYAN}${THIS_BRANCH}${NORMAL}'? (y/n) "
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # Create new branch
+        #printf "Creating ${YELLOW}${NEXT_BRANCH_NAME}${NORMAL}...\n"
+        BRANCH_COMMENT="#${BRANCH_NO} comment: Rebaseline ${THIS_BRANCH}"
+        svn copy ${URL_TRUNK_ROOT} ${URL_BRANCH_ROOT}${NEXT_BRANCH_NAME} -m "$BRANCH_COMMENT"
+        #printf "Created ${YELLOW}${NEXT_BRANCH_NAME}${NORMAL} with branch comment: '${BRANCH_COMMENT}'\n\n"
+
+        # Switch to new branch
+        printf "Switching to ${YELLOW}${NEXT_BRANCH_NAME}${NORMAL}...\n"
+        sb $NEXT_BRANCH_NAME
+
+        # Test merge with previous version
+        REVISIONS_TO_MERGE=$(getRevisionCMD $THIS_BRANCH)
+
+        printf "\n${GREEN}Test merge:${NORMAL}\n"
+        printf "${MAGENTA}Running:${NORMAL} svn merge --dry-run -r $REVISIONS_TO_MERGE ${URL_BRANCH_ROOT}${THIS_BRANCH}\n"
+
+        svn merge --dry-run -r $REVISIONS_TO_MERGE ${URL_BRANCH_ROOT}${THIS_BRANCH}
+
+        # Merge for good
+        printf "\n"
+        read -p "Merge and commit? (y/n) "
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            printf "\n${GREEN}Merge:${NORMAL}\n"
+            # Merge it so!
+            svn merge -r $REVISIONS_TO_MERGE ${URL_BRANCH_ROOT}${THIS_BRANCH}
+
+            printf "\n"
+            # Commit!
+            BRANCH_NUM=$(getBranchNumberFromName ${THIS_BRANCH})
+            REVISIONS_FOR_COMMENT=$(getRevisionRangeForComment $THIS_BRANCH)
+            COMMIT_MSG="#${BRANCH_NUM} comment: Merged revisions [${REVISIONS_FOR_COMMENT}] from ${THIS_BRANCH}"
+            printf "\n"
+            commit "$COMMIT_MSG" --quiet
+        else
+            return 0
+        fi
+    else
+        return 0
+    fi
+}
+
+function findBranch() 
+{
+    if [ $# -ge 1 ]
+    then
+        sites
+        branchArr=($(svn ls https://trac.fg123.co.uk/svn/elastic/branches | grep "$1"))
+        NUM=0
+        for branch in ${!branchArr[@]}; do
+            ((NUM++))
+            echo "($NUM) ${CYAN}${branchArr[branch]}${NORMAL}"
+        done
+        printf "\nChoose a branch to switch to, or (0) to quit: ";
+        read branchNum
+        if [ $branchNum -eq 0 ]
+        then
+            return 0
+        else
+            ((branchNum--))
+            printf "Switching to: ${CYAN}${branchArr[branchNum]}${NORMAL}\n"
+            sb ${branchArr[branchNum]}
+            return 1
+        fi
+    else
+		printf "$MSG_FAIL Search pattern required.\n$MSG_USAGE fb hotfix.\n"
+    fi
+}
+export -f findBranch
+alias fb='findBranch'
 export PATH=$PATH:/lib/:/lib/node_modules/npm/bin/:/usr/bin/phpunit
 export SVN_EDITOR=vim
 
@@ -360,3 +582,4 @@ fi
 if [ -f /etc/bash_completion.d/nb ]; then
 	. /etc/bash_completion.d/nb
 fi
+
