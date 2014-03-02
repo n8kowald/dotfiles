7f4fc62a18bcc5aeec737393df2141bf1c31d4cc
Nathan Kowald <nathankowald@gmail.com>
2014-03-02 13:24:14 +0000

    Add colours into think-finance shell script

--- bashrc_modules/think-finance.sh
+++ bashrc_modules/think-finance.sh
@@ -4,6 +4,21 @@
 # Think Finance Specific Bash
 #----------------------------------
 
+# Colours!
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
 # Global variables
 EXAMPLE_BRANCH="123456_branch_name_1"
 MSG_USAGE="${YELLOW}Usage:${NORMAL}"
